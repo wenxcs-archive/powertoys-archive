cb5e3b7cafd0cb20144f65210f6a051b956b8554
Wenxiang <8460860+wenxcs@users.noreply.github.com>
2024-06-09 00:12:55 +0800

    Create setup_vim-unix_like.sh

---
+++ vim/setup_vim-unix_like.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+script_dir=$(dirname "$0")
+
+if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
+    echo "Installing vim-plug"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if [[ ! -d ~/.vim/pack/github/start/copilot.vim ]]; then
+    echo "Installing copilot.vim"
+    git clone https://github.com/github/copilot.vim.git ~/.vim/pack/github/start/copilot.vim
+fi 
+
+curl -fLo ~/.vimrc --create-dirs https://raw.githubusercontent.com/wenxcs/powertoys/master/vim/vimrc
+
+vim -c "PlugInstall" -c "qa" > /dev/null 2>&1
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wenxcs/powertoys/master/vim/setup_vim-unix_like.sh)"
